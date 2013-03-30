b05860d71d78f2a6fe9b225bb5c4c241e2f2f8d5
Abdella <abdellaalioncan@gmail.com>
2013-03-30 14:11:50 -0400

    Added dog

---
+++ GitTest/GitTest/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest
+{
+    class Dog:IAnimal
+    {
+        string name = "Dog";
+        bool iAmFurry = true;
+        int toughness = 7;
+    }
+}
