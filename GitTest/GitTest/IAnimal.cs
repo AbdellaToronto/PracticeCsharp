47a2a48bf9b516bcb92d6e53e93b68933da949e5
Abdella <abdellaalioncan@gmail.com>
2013-03-30 13:57:29 -0400

    Created VS app
    also created the interface, 'IAnimal', which will be used to make
    animals

---
+++ GitTest/GitTest/IAnimal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest
+{
+    interface IAnimal
+    {
+        string name;
+        bool amIFurry;
+        int toughness;
+
+        public void speak(string userName)
+        {
+            Console.WriteLine("I'm a {0}, and my toughness level is {1}. Your name is {2}",name, toughness,userName);
+        }
+    }
+}
